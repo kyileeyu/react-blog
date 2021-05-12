284496190d536aa927e8422b262a5a5bf420082c
Kyilee <kyileeeyu@gmail.com>
2021-05-12 14:56:23 +0900

    add : change button

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -1,25 +1,72 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [ 글제목, 글제목변경 ] = useState(['남자봄패션 코디','서촌나들이','패턴이 바꼈다']); //array 가 들어감
+  let [ 따봉, 따봉변경 ] = useState(0);
+
+
+  function 제목바꾸기(){//state를 딥카피해서 변경-reference data type참조형 자료형
+    var newArray = [...글제목]; //deep copy =서로 독립적인 값을 가지는것을 복사  직접 수정이 아닌 deep copy
+    newArray[0] = '여자봄패션 코디';
+    글제목변경( newArray );
+
+  }
+
+  function 순서바꾸기(){//state를 딥카피해서 변경-reference data type참조형 자료형
+    var newArray = [...글제목]; //deep copy =서로 독립적인 값을 가지는것을 복사  직접 수정이 아닌 deep copy
+    var tmp = newArray[0]
+    newArray[0] = newArray[1];
+    newArray[1] = tmp;
+    글제목변경( newArray );
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <button onClick={ 제목바꾸기 }>버튼</button>
+      <button onClick={ 순서바꾸기 }>버튼</button>
+      <div className="list">
+        <ul>
+          <li>
+            <h4>{ 글제목[0] }
+            <span onClick={ ()=>{따봉변경(따봉+1)} }> ❤️ </span> { 따봉 }
+              </h4>
+            <p>5월 11일 발행</p>
+            <hr/>
+          </li>
+          <li>
+            <h4>{ 글제목[1] }</h4>
+            <p>5월 10일 발행</p>
+            <hr/>
+          </li>
+          <li>
+            <h4>{ 글제목[2] }</h4>
+            <p>5월 9일 발행</p>
+            <hr/>
+          </li>
+        </ul>
+      </div>
+
+      <Modal></Modal>
+
+    </div> 
   );
 }
+function Modal(){//componant
+  return(
+    <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+  )
+}
+
 
 export default App;
