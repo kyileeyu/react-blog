27ccc154c939e7a96c8967861cbe39b2e226c5d1
Kyilee <kyileeeyu@gmail.com>
2021-05-15 18:56:40 +0900

    state,component 사용, map&props 배움

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 function App() {
 
   let [ 글제목, 글제목변경 ] = useState(['남자봄패션 코디','서촌나들이','패턴이 바꼈다']); //array 가 들어감
-  let [ 따봉, 따봉변경 ] = useState(0);
-
-
+  let [ 따봉, 따봉변경 ] = useState( [0,0,0,0] );
+  let [modal, modal변경] = useState(false);
+  let [누른제목, 누른제목변경] = useState(0);
   function 제목바꾸기(){//state를 딥카피해서 변경-reference data type참조형 자료형
     var newArray = [...글제목]; //deep copy =서로 독립적인 값을 가지는것을 복사  직접 수정이 아닌 deep copy
     newArray[0] = '여자봄패션 코디';
@@ -26,38 +26,73 @@ function App() {
 
   return (
     <div className="App">
+
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+
       <button onClick={ 제목바꾸기 }>버튼</button>
       <button onClick={ 순서바꾸기 }>버튼</button>
+
+
       <div className="list">
-        <ul>
-          <li>
-            <h4>{ 글제목[0] }
-            <span onClick={ ()=>{따봉변경(따봉+1)} }> ❤️ </span> { 따봉 }
-              </h4>
-            <p>5월 11일 발행</p>
-            <hr/>
-          </li>
-          <li>
-            <h4>{ 글제목[1] }</h4>
-            <p>5월 10일 발행</p>
-            <hr/>
-          </li>
-          <li>
-            <h4>{ 글제목[2] }</h4>
-            <p>5월 9일 발행</p>
-            <hr/>
-          </li>
-        </ul>
+        <h4>{ 글제목[0] }
+        <span onClick={ ()=>{따봉변경(따봉+1)} }> ❤️ </span> { 따봉 }
+        </h4>
+        <p>5월 11일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>5월 10일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4 onClick={ ()=>{ modal변경(true) } }> { 글제목[2] } </h4>
+        <p onClick={ ()=>{ modal변경(true) } }>5월 9일 발행</p>
+        { 
+         modal === true 
+         ? <Modal />
+         : null
+        }
+        <hr/>
       </div>
 
-      <Modal></Modal>
 
+        {
+          글제목.map(function(i){
+            return (
+              <div className="list">
+                <h4>{ i } <span onClick={ ()=>{따봉변경(따봉+1)} }> ❤️ </span> { 따봉 }</h4>
+                <p>5월 10일 발행</p>
+                <hr/>
+              </div>
+          )})
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <button onClick={ ()=>{ modal변경(!modal) } }> 모달 </button>
+      { 
+         modal === true 
+         ? <Modal />
+         : null
+      }
     </div> 
   );
 }
+ 
 function Modal(){//componant
   return(
     <div className="modal">
