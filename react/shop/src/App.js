bf86d2f50f0975149f94e8129d0e692291847a7f
Kyilee <kyileeeyu@gmail.com>
2021-05-20 23:15:26 +0900

    Add: router

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import React,{ useState } from 'react';
 import data from './data.js';
  
+import { Link, Route, Switch } from 'react-router-dom';
+
 function App() {
 
   let [shoes,shoesCH] = useState(data);
@@ -27,28 +29,49 @@ function App() {
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-    <Jumbotron className="background">
-      <h1>20% Season OFF</h1>
-      <p>
-        This is a simple hero unit, a simple jumbotron-style component for calling
-        extra attention to featured content or information.
-      </p>
-      <p>
-        <Button variant="primary">Learn more</Button>
-      </p>
-    </Jumbotron>
+ 
     
-    <div className="Container">
-      <div className="row">
+        
+    <Route exact path="/">
+
+      <Jumbotron className="background">
+        <h1>20% Season OFF</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
+
+      <div className="Container">
+        <div className="row">
         { 
           shoes.map((a,i)=>{
-            return <Card shoes={shoes[i]} i={i} />
+            return <Card shoes={shoes[i]} i={i} key={i}/>
           })
         }
-
-       
+        </div>
       </div>
-    </div>
+    </Route>
+
+    <Route path="/detail">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+          </div>
+          <div className="col-md-6 mt-4">
+            <h4 className="pt-5">상품명</h4>
+            <p>상품설명</p>
+            <p>120000원</p>
+            <button className="btn btn-danger">주문하기</button> 
+          </div>
+        </div>
+      </div> 
+    </Route>
+
   </div>
   )
 }
