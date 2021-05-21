cf1530989fc74bde60ec8a24727c3dee78ca5431
Kyilee <kyileeeyu@gmail.com>
2021-05-21 23:58:54 +0900

    Add: Link,history

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -2,13 +2,14 @@ import { Navbar,Nav,NavDropdown,Button,Jumbotron, Container } from 'react-bootst
 import logo from './logo.svg';
 import './App.css';
 import React,{ useState } from 'react';
-import data from './data.js';
+import Data from './Data.js';
+import Detail from './Detail.js';
  
 import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
 
-  let [shoes,shoesCH] = useState(data);
+  let [shoes,shoesCH] = useState(Data);
 
   return (
     <div className="App">
@@ -17,8 +18,8 @@ function App() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link> <Link to="/">Home</Link></Nav.Link>
+          <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -56,20 +57,8 @@ function App() {
       </div>
     </Route>
 
-    <Route path="/detail">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-          </div>
-          <div className="col-md-6 mt-4">
-            <h4 className="pt-5">상품명</h4>
-            <p>상품설명</p>
-            <p>120000원</p>
-            <button className="btn btn-danger">주문하기</button> 
-          </div>
-        </div>
-      </div> 
+    <Route path="/detail/:id">
+        <Detail shoes={shoes}/>
     </Route>
 
   </div>
